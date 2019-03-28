bd45b0b843c0175cce85c6795dc1056a1d3175a3
zhangzhenyu <1299431510@qq.com>
2019-03-28 10:02:41 +0800

    zhangzhenyu-修改router-3.28

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 
 import Index from "../index/index"
@@ -7,35 +7,52 @@ import Login from "../login/login"
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
 
+class BasicRoute extends Component {
+    //构造函数
+    constructor(props) {
+        super(props);
+        this.state = {
 
-const BasicRoute = () => (
-    <BrowserRouter>
-            <div>
-                <Header />
+        };
+    }
+    //挂载
+    componentDidMount() {
+
+    }
+    //卸载
+    componentWillUnmount() {
+
+    }
+    render() {
+        return(
+            <BrowserRouter>
                 <div>
-                    <ul>
-						<li>
-                            <Link to="/">index</Link>
-						</li>
-                        <li>
-                            <Link to="/details">details</Link>
-						</li>
-                        <li>
-                            <Link to="/login">login</Link>
-                        </li>
-                    </ul>
+                    <Header />
                     <div>
-                        <div>变化区域↓</div>
-                        <Switch>
-                            <Route exact path="/" component={Index} />
-                            <Route exact path="/details" component={Details} />
-                            <Route exact path="/login" component={Login} />
-                        </Switch>
+                        <ul>
+                            <li>
+                                <Link to="/">index</Link>
+                            </li>
+                            <li>
+                                <Link to="/details">details</Link>
+                            </li>
+                            <li>
+                                <Link to="/login">login</Link>
+                            </li>
+                        </ul>
+                        <div>
+                            <div>变化区域↓</div>
+                            <Switch>
+                                <Route exact path="/" component={Index} />
+                                <Route exact path="/details" component={Details} />
+                                <Route exact path="/login" component={Login} />
+                            </Switch>
+                        </div>
                     </div>
+                    <Footer />
                 </div>
-                <Footer />
-            </div>
-    </BrowserRouter>
-);
-
+            </BrowserRouter>
+        );
+    }
+}
 export default BasicRoute;
